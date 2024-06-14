3634bc7b3ff506e9dedecfd2e89960149c06011a
pt1C3 <rafaelsilva1c3@gmail.com>
2024-06-14 16:01:42 +0100

    várias cenas

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -8,7 +8,13 @@ public class ChangeScene : MonoBehaviour
 
     public void ChangeSceneAction(string sceneName)
     {
+        StartCoroutine(waitChange(sceneName));
+    }
+    IEnumerator waitChange(string sceneName)
+    {
+        yield return new WaitForSeconds(.2f);
         SceneManager.LoadScene(sceneName);
+
     }
 
     public void UnloadLoseScene()
